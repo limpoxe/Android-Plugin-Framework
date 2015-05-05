af90207b9a5813a5d4a7354dde536db66cb7c827
Cai Liming <405977146@163.com>
2015-05-05 18:12:17 +0800

    添加对插件中FragmentActivity的测试demo
    Signed-off-by: Cai Liming <405977146@163.com>

--- PluginMain/src/com/example/pluginmain/PluginListActivity.java
+++ PluginMain/src/com/example/pluginmain/PluginListActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.plugin.core.PluginDescriptor;
@@ -25,21 +26,29 @@ public class PluginListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-
+		LinearLayout root = (LinearLayout) findViewById(R.id.root);
+		TextView text = new TextView(this);
+		text.setText("注意：插件Fragment如果运行在和它的开发方式不匹配的运行容器中，会出错，\n" +
+				"" +
+				"如下列表中会对这种异常进行演示");
+		text.setTextSize(16);
+		root.addView(text);
+		
 		// 这行代码应当在Application的onCreate中执行。
 		PluginLoader.initLoader(getApplication());
 
 		HashMap<String, PluginDescriptor> plugins = PluginLoader.listAll();
 
-		LinearLayout root = (LinearLayout) findViewById(R.id.root);
 		Iterator<Entry<String, PluginDescriptor>> it = plugins.entrySet().iterator();
 		while (it.hasNext()) {
+			
+			//列出fragment
 			final Entry<String, PluginDescriptor> item = it.next();
 			Iterator<Entry<String, String>> itemItr = item.getValue().getFragments().entrySet().iterator();
 			while (itemItr.hasNext()) {
 				final Entry<String, String> itemItem = itemItr.next();
 				Button btn = new Button(this);
-				btn.setText(itemItem.getKey() + " " + itemItem.getValue());
+				btn.setText(itemItem.getKey() + " " + itemItem.getValue() + "运行在Normal中");
 				btn.setOnClickListener(new OnClickListener() {
 
 					@Override
@@ -47,10 +56,13 @@ public class PluginListActivity extends Activity {
 						PluginDispatcher.startFragment(PluginListActivity.this, itemItem.getKey());
 					}
 				});
-				root.addView(btn);
+				LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 
+						LinearLayout.LayoutParams.WRAP_CONTENT);
+				llp.setMargins(4, 14, 4, 14);
+				root.addView(btn, llp);
 
 				btn = new Button(this);
-				btn.setText(itemItem.getKey() + " " + itemItem.getValue());
+				btn.setText(itemItem.getKey() + " " + itemItem.getValue() + "运行在Spec中");
 				btn.setOnClickListener(new OnClickListener() {
 
 					@Override
@@ -58,19 +70,31 @@ public class PluginListActivity extends Activity {
 						PluginDispatcher.startNormalFragment(PluginListActivity.this, itemItem.getKey());
 					}
 				});
-				root.addView(btn);
+				llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 
+						LinearLayout.LayoutParams.WRAP_CONTENT);
+				llp.setMargins(4, 14, 4, 14);
+				root.addView(btn, llp);
 			}
 
-			Button btn = new Button(this);
-			btn.setText("test3" + " : com.example.plugintest.PluginTextActivity ");
-			btn.setOnClickListener(new OnClickListener() {
+			//列出activity
+			itemItr = item.getValue().getActivities().entrySet().iterator();
+			while (itemItr.hasNext()) {
+				final Entry<String, String> itemItem = itemItr.next();
+				Button btn = new Button(this);
+				btn.setText(itemItem.getKey() + " " + itemItem.getValue() + "运行在Proxy中");
+				btn.setOnClickListener(new OnClickListener() {
 
-				@Override
-				public void onClick(View v) {
-					PluginDispatcher.startActivity(PluginListActivity.this, "test3");
-				}
-			});
-			root.addView(btn);
+					@Override
+					public void onClick(View v) {
+						PluginDispatcher.startActivity(PluginListActivity.this, itemItem.getKey());
+					}
+				});
+				LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 
+						LinearLayout.LayoutParams.WRAP_CONTENT);
+				llp.setMargins(4, 14, 4, 14);
+				root.addView(btn, llp);
+ 
+			}
 
 		}
 	}
