142916a8546f7d73c350959796c1b76b0169c7ed
Cai Liming <405977146@163.com>
2015-05-08 15:00:06 +0800

    更新插件列表展示界面 Signed-off-by: Cai Liming <405977146@163.com>

---
+++ PluginMain/src/com/example/pluginmain/PluginDetailActivity.java
@@ -0,0 +1,84 @@
+package com.example.pluginmain;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.plugin.core.PluginDescriptor;
+import com.plugin.core.PluginLoader;
+import com.plugin.core.ui.PluginDispatcher;
+
+public class PluginDetailActivity extends Activity {
+	
+	private ViewGroup mRoot;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail_activity);
+		mRoot = (ViewGroup) findViewById(R.id.root);
+		
+		String pluginId = getIntent().getStringExtra("plugin_id");
+		PluginDescriptor pluginDescriptor = PluginLoader.getPluginDescriptorByPluginId(pluginId);
+		
+		initViews(pluginDescriptor);
+	}
+	 
+	private void initViews(PluginDescriptor pluginDescriptor) {
+		if (pluginDescriptor != null) {
+			TextView pluginIdView = (TextView)mRoot.findViewById(R.id.plugin_id);
+			pluginIdView.setText("插件Id:" + pluginDescriptor.getId());
+			
+			TextView pluginVerView = (TextView)mRoot.findViewById(R.id.plugin_version);
+			pluginVerView.setText("插件Version:" + pluginDescriptor.getVersion());
+			
+			LinearLayout pluginFragmentView = (LinearLayout)mRoot.findViewById(R.id.plugin_fragments);
+			Iterator<Entry<String, String>> Fragment = pluginDescriptor.getFragments().entrySet().iterator();
+			while (Fragment.hasNext()) {
+				final Entry<String, String> entry = Fragment.next();
+				TextView tv = new TextView(this);
+				tv.setText("插件ClassId：" + entry.getKey());
+				tv.append("\n插件ClassName：" + entry.getValue());
+				tv.append("\n插件Class类型：Fragment");
+				tv.append("\n点击打开>>");
+				tv.append("\n");
+				tv.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						//PluginDispatcher.startFragment(PluginDetailActivity.this, entry.getKey());
+						PluginDispatcher.startNormalFragment(PluginDetailActivity.this, entry.getKey());
+					}
+				});
+				pluginFragmentView.addView(tv);
+			}
+			
+			LinearLayout pluginActivitysView = (LinearLayout)mRoot.findViewById(R.id.plugin_activities);
+			Iterator<Entry<String, String>> activity = pluginDescriptor.getActivities().entrySet().iterator();
+			while (activity.hasNext()) {
+				final Entry<String, String> entry = activity.next();
+				TextView tv = new TextView(this);
+				tv.setText("插件ClassId：" + entry.getKey());
+				tv.append("\n插件ClassName：" + entry.getValue());
+				tv.append("\n插件Class类型：Activity");
+				tv.append("\n点击打开插件：");
+				tv.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						PluginDispatcher.startActivity(PluginDetailActivity.this, entry.getKey());
+					}
+				});
+				pluginActivitysView.addView(tv);
+			}
+			
+		}
+	}
+}
--- PluginMain/src/com/example/pluginmain/PluginListActivity.java
+++ PluginMain/src/com/example/pluginmain/PluginListActivity.java
@@ -11,19 +11,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.plugin.core.PluginDescriptor;
 import com.plugin.core.PluginLoader;
-import com.plugin.core.ui.PluginDispatcher;
 
 public class PluginListActivity extends Activity {
 	
@@ -49,89 +44,36 @@ public class PluginListActivity extends Activity {
 	
 	private void listAll(ViewGroup root) {
 		root.removeAllViews();
-		
-		HashMap<String, PluginDescriptor> plugins = PluginLoader.listAll();
-		
-		TextView text = new TextView(this);
-		text.setText("注意：插件Fragment如果运行在和它的开发方式不匹配的运行容器中，会出错。" +
-				"\n" +
-				"如下列表中会对这种异常进行演示: " + plugins.size() + "个插件");
-		text.setTextSize(16);
-		root.addView(text);
+		TextView lable = new TextView(this);
+		lable.setText("已安装的插件列表：");
+		lable.setTextSize(16);
+		root.addView(lable);
 		
 		//列出所有已经安装的插件
-
-		Iterator<Entry<String, PluginDescriptor>> it = plugins.entrySet().iterator();
-		while (it.hasNext()) {
+		HashMap<String, PluginDescriptor> plugins = PluginLoader.listAll();
+		Iterator<Entry<String, PluginDescriptor>> itr = plugins.entrySet().iterator();
+		while (itr.hasNext()) {
+			Entry<String, PluginDescriptor> entry  = itr.next();
+			Button btton = new Button(this);
+			btton.setText("插件id：" + entry.getKey() + 
+					"\n插件介绍：" + entry.getValue().getDescription() +
+					"\n安装路径：" + entry.getValue().getInstalledPath() +
+					"\n点击查看详情>>");
+			LayoutParams layoutParam = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			layoutParam.topMargin = 10;
+			layoutParam.bottomMargin = 10;
+			root.addView(btton, layoutParam);
 			
-			//列出fragment
-			final Entry<String, PluginDescriptor> item = it.next();
-			Iterator<Entry<String, String>> itemItr = item.getValue().getFragments().entrySet().iterator();
-			while (itemItr.hasNext()) {
-				final Entry<String, String> itemItem = itemItr.next();
-				Button btn = new Button(this);
-				btn.setText(itemItem.getKey() + " " + itemItem.getValue() + "运行在Normal中");
-				btn.setOnClickListener(new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						PluginDispatcher.startFragment(PluginListActivity.this, itemItem.getKey());
-					}
-				});
-				LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 
-						LinearLayout.LayoutParams.WRAP_CONTENT);
-				llp.setMargins(4, 14, 4, 14);
-				root.addView(btn, llp);
-
-				btn = new Button(this);
-				btn.setText(itemItem.getKey() + " " + itemItem.getValue() + "运行在Spec中");
-				btn.setOnClickListener(new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						PluginDispatcher.startNormalFragment(PluginListActivity.this, itemItem.getKey());
-					}
-				});
-				llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 
-						LinearLayout.LayoutParams.WRAP_CONTENT);
-				llp.setMargins(4, 14, 4, 14);
-				root.addView(btn, llp);
-			}
-
-			//列出activity
-			itemItr = item.getValue().getActivities().entrySet().iterator();
-			while (itemItr.hasNext()) {
-				final Entry<String, String> itemItem = itemItr.next();
-				Button btn = new Button(this);
-				btn.setText(itemItem.getKey() + " " + itemItem.getValue() + "运行在Proxy中");
-				btn.setOnClickListener(new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						PluginDispatcher.startActivity(PluginListActivity.this, itemItem.getKey());
-					}
-				});
-				LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 
-						LinearLayout.LayoutParams.WRAP_CONTENT);
-				llp.setMargins(4, 14, 4, 14);
-				root.addView(btn, llp);
- 
-			}
-
+			
+			btton.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent intent  = new Intent(PluginListActivity.this, PluginDetailActivity.class);
+					startActivity(intent);
+				}
+			});
 		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.add(0,0, 0, "test menu");
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		Toast.makeText(this, "text", Toast.LENGTH_LONG).show();
-		Log.e("xx", "" + item.getTitle());
-		return super.onOptionsItemSelected(item);
 	}
 	
 	private BroadcastReceiver pluginChange = new BroadcastReceiver() {
