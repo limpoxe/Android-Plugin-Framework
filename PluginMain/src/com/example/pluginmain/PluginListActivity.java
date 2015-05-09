1a097cab5357d5e30beb362c7dd46c0f9f30c95e
limpoxe <405977146@163.com>
2015-05-09 10:36:28 +0800

    organize import
    Signed-off-by: limpoxe <405977146@163.com>

--- PluginMain/src/com/example/pluginmain/PluginListActivity.java
+++ PluginMain/src/com/example/pluginmain/PluginListActivity.java
@@ -2,7 +2,6 @@ package com.example.pluginmain;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map.Entry;
@@ -14,43 +13,42 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.TextView;
 
 import com.plugin.core.PluginDescriptor;
 import com.plugin.core.PluginLoader;
 import com.plugin.util.ApkReader;
 
 public class PluginListActivity extends Activity {
-	
+
 	private ViewGroup mList;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-		
+
 		// 这行代码应当在Application的onCreate中执行。
 		PluginLoader.initLoader(getApplication());
-		
+
 		registerReceiver(pluginChange, new IntentFilter(PluginLoader.ACTION_PLUGIN_CHANGED));
-		
+
 		Button install = (Button) findViewById(R.id.install);
 		install.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Toast.makeText(PluginListActivity.this, "开始安装", Toast.LENGTH_LONG).show();
 				try {
 					InputStream assestInput = getAssets().open("PluginTest-debug.apk");
-					String sdcardDest = Environment.getExternalStorageDirectory().getAbsolutePath() + "/PluginTest-debug.apk";
+					String sdcardDest = Environment.getExternalStorageDirectory().getAbsolutePath()
+							+ "/PluginTest-debug.apk";
 					if (ApkReader.copyFile(assestInput, sdcardDest)) {
-						PluginLoader.installPlugin(sdcardDest);						
+						PluginLoader.installPlugin(sdcardDest);
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -58,51 +56,50 @@ public class PluginListActivity extends Activity {
 				}
 			}
 		});
-		
-		
+
 		mList = (ViewGroup) findViewById(R.id.list);
-		
+
 		listAll(mList);
 	}
-	
+
 	private void listAll(ViewGroup root) {
 		root.removeAllViews();
-		
-		//列出所有已经安装的插件
+
+		// 列出所有已经安装的插件
 		Hashtable<String, PluginDescriptor> plugins = PluginLoader.listAll();
 		Iterator<Entry<String, PluginDescriptor>> itr = plugins.entrySet().iterator();
 		while (itr.hasNext()) {
-			final Entry<String, PluginDescriptor> entry  = itr.next();
+			final Entry<String, PluginDescriptor> entry = itr.next();
 			Button btton = new Button(this);
-			btton.setText("插件id：" + entry.getKey() + 
-					"\n插件介绍：" + entry.getValue().getDescription() +
-					"\n安装路径：" + entry.getValue().getInstalledPath() +
-					"\n点击查看详情>>");
+			btton.setText("插件id：" + entry.getKey() + "\n插件介绍：" + entry.getValue().getDescription() + "\n安装路径："
+					+ entry.getValue().getInstalledPath() + "\n点击查看详情>>");
 			LayoutParams layoutParam = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 			layoutParam.topMargin = 10;
 			layoutParam.bottomMargin = 10;
 			root.addView(btton, layoutParam);
-			
-			
+
 			btton.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
-					Intent intent  = new Intent(PluginListActivity.this, PluginDetailActivity.class);
+					Intent intent = new Intent(PluginListActivity.this, PluginDetailActivity.class);
 					intent.putExtra("plugin_id", entry.getKey());
 					startActivity(intent);
 				}
 			});
 		}
 	}
-	
-	private BroadcastReceiver pluginChange = new BroadcastReceiver() {
+
+	private final BroadcastReceiver pluginChange = new BroadcastReceiver() {
+		@Override
 		public void onReceive(Context context, Intent intent) {
-			Toast.makeText(PluginListActivity.this, "插件" + intent.getStringExtra(PluginLoader.EXTRA_TYPE)  +"成功", Toast.LENGTH_LONG).show();
+			Toast.makeText(PluginListActivity.this, "插件" + intent.getStringExtra(PluginLoader.EXTRA_TYPE) + "成功",
+					Toast.LENGTH_LONG).show();
 			listAll(mList);
 		};
 	};
-	
+
+	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		unregisterReceiver(pluginChange);
