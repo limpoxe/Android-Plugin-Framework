cbfb7db585608471eb9c69df7c051a2c2f0c6e8f
Cai Liming <405977146@163.com>
2015-05-13 15:13:11 +0800

    fix demo bug
    Signed-off-by: Cai Liming <405977146@163.com>

--- PluginMain/src/com/example/pluginmain/PluginDetailActivity.java
+++ PluginMain/src/com/example/pluginmain/PluginDetailActivity.java
@@ -73,10 +73,10 @@ public class PluginDetailActivity extends Activity {
 					@Override
 					public void onClick(View v) {
 						//两种fragment模式
-						PluginDispatcher.startFragmentWithSimpleActivity(PluginDetailActivity.this, entry.getKey());
-						if (entry.getKey().equals("test1")) {
-							PluginDispatcher.startFragmentWithBuildInActivity(PluginDetailActivity.this, entry.getKey());
+						if (!entry.getKey().equals("test1")) {
+							PluginDispatcher.startFragmentWithSimpleActivity(PluginDetailActivity.this, entry.getKey());
 						}
+						PluginDispatcher.startFragmentWithBuildInActivity(PluginDetailActivity.this, entry.getKey());
 					}
 				});
 				pluginFragmentView.addView(btn);
