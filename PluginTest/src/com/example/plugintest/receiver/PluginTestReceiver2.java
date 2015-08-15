c0c1eee7c7cec83915de2f284d47c6a9180a3e36
limpoxe <405977146@163.com>
2015-08-15 10:49:29 +0800

    Update Demo for Receiver
    Signed-off-by: limpoxe <405977146@163.com>

---
+++ PluginTest/src/com/example/plugintest/receiver/PluginTestReceiver2.java
@@ -0,0 +1,25 @@
+package com.example.plugintest.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * 插件receiver不能监听系统广播
+ * 
+ * @author cailiming
+ * 
+ */
+public class PluginTestReceiver2 extends BroadcastReceiver {
+
+	/**
+	 * 这个context是主程序的Context，因此不能使用这个context去访问当前插件的资源
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		Toast.makeText(context, "onReceive2 " + intent.toUri(0), Toast.LENGTH_LONG).show();
+	}
+
+}
