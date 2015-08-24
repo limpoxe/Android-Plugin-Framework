704450ba489cd1157cbda75454684204fbea2ddf
limpoxe <405977146@163.com>
2015-08-24 18:31:01 +0800

    添加WebView测试demo
    Signed-off-by: limpoxe <405977146@163.com>

---
+++ PluginTest/src/com/example/plugintest/activity/PluginWebViewActivity.java
@@ -0,0 +1,80 @@
+package com.example.plugintest.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebSettings.PluginState;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.plugintest.R;
+
+public class PluginWebViewActivity extends Activity implements OnClickListener {
+	WebView web;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.plugin_webview);
+
+		Button bn = (Button) findViewById(R.id.btn);
+
+		bn.setOnClickListener(this);
+
+		web = (WebView) findViewById(R.id.webview);
+		setUpWebViewSetting();
+		setClient();
+	}
+
+	@Override
+	public void onClick(View v) {
+		web.loadUrl("http://www.baidu.com/");
+	}
+
+	private void setUpWebViewSetting() {
+		WebSettings webSettings = web.getSettings();
+
+		webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);// 根据cache-control决定是否从网络上取数据
+		webSettings.setSupportZoom(true);
+		webSettings.setBuiltInZoomControls(true);// 显示放大缩小
+		webSettings.setJavaScriptEnabled(true);
+		// webSettings.setPluginsEnabled(true);
+		webSettings.setPluginState(PluginState.ON);
+		webSettings.setUserAgentString(webSettings.getUserAgentString());
+		webSettings.setDomStorageEnabled(true);
+		webSettings.setAppCacheEnabled(true);
+		webSettings.setAppCachePath(getCacheDir().getPath());
+		webSettings.setUseWideViewPort(true);// 影响默认满屏和双击缩放
+		webSettings.setLoadWithOverviewMode(true);// 影响默认满屏和手势缩放
+
+	}
+
+	private void setClient() {
+
+		web.setWebChromeClient(new WebChromeClient() {
+		});
+
+		// 如果要自动唤起自定义的scheme，不能设置WebViewClient，
+		// 否则，需要在shouldOverrideUrlLoading中自行处理自定义scheme
+		// webView.setWebViewClient();
+		web.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+
+		});
+	}
+}
