253452f53102734bce827abed081cbb98f506790
limpoxe <405977146@163.com>
2015-09-19 15:31:20 +0800

    清理测试代码

--- PluginMain/src/com/example/pluginmain/PluginListActivity.java
+++ PluginMain/src/com/example/pluginmain/PluginListActivity.java
@@ -47,41 +47,6 @@ public class PluginListActivity extends Activity {
 
 	private void initView() {
 
-		Button check = (Button) findViewById(R.id.check);
-		check.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
-				//ActivityThread检查Instrumentation是否已经替换成功了
-				Object activityThread = RefInvoker.invokeStaticMethod("android.app.ActivityThread", "currentActivityThread",
-						(Class[]) null, (Object[]) null);
-				Instrumentation originalInstrumentation = (Instrumentation) RefInvoker.getFieldObject(activityThread,
-						"android.app.ActivityThread", "mInstrumentation");
-
-				Toast.makeText(PluginListActivity.this,
-						originalInstrumentation == null?"activityThread null":(originalInstrumentation.getClass().getName() + ", " + (originalInstrumentation instanceof PluginInstrumentionWrapper)),
-						Toast.LENGTH_LONG ).show();
-
-				//如果发现没有被替换，再次尝试替换一下
-				if (originalInstrumentation != null && !(originalInstrumentation instanceof PluginInstrumentionWrapper)) {
-					RefInvoker.setFieldObject(activityThread, "android.app.ActivityThread", "mInstrumentation",
-							new PluginInstrumentionWrapper(originalInstrumentation));
-				}
-
-
-				//Activity检查Instrumentation是否已经替换成功了
-				Instrumentation activityInstrumentation = (Instrumentation) RefInvoker.getFieldObject(PluginListActivity.this,
-						Activity.class.getName(), "mInstrumentation");
-
-				Toast.makeText(PluginListActivity.this,
-						activityInstrumentation == null?"Activity null":(activityInstrumentation.getClass().getName() + ", " + (activityInstrumentation instanceof PluginInstrumentionWrapper)),
-						Toast.LENGTH_LONG ).show();
-
-
-			}
-		});
-
-
 		install = (Button) findViewById(R.id.install);
 		install.setOnClickListener(new View.OnClickListener() {
 
