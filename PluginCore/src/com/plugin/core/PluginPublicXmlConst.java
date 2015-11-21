54472f3fe0958295232030d65235d94028575676
cailiming <liming.clm@alibaba-inc.com>
2015-11-21 23:49:29 +0800

    常量调整

--- PluginCore/src/com/plugin/core/PluginPublicXmlConst.java
+++ PluginCore/src/com/plugin/core/PluginPublicXmlConst.java
@@ -3,42 +3,42 @@ package com.plugin.core;
 import android.util.SparseArray;
 
 /**
- * For eclipse with public.xml
+ * Consts in public.xml
  */
 public class PluginPublicXmlConst {
-	public static final int main_attr = 0x7f01;
-	public static final int main_drawable = 0x7f02;
-	public static final int main_layout = 0x7f03;
-	public static final int main_anim = 0x7f04;
-	public static final int main_xml = 0x7f05;
-	public static final int main_raw = 0x7f06;
-	public static final int main_dimen = 0x7f07;
-	public static final int main_string = 0x7f08;
-	public static final int main_style = 0x7f09;
-	public static final int main_color = 0x7f0a;
-	public static final int main_id = 0x7f0b;
-	public static final int main_bool = 0x7f0c;
-	public static final int main_int = 0x7f0d;
-	public static final int main_array = 0x7f0e;
-	public static final int main_menu = 0x7f0f;
+	public static final int public_static_final_host_attr = 0x7f31;
+	public static final int public_static_final_host_drawable = 0x7f32;
+	public static final int public_static_final_host_layout = 0x7f33;
+	public static final int public_static_final_host_anim = 0x7f34;
+	public static final int public_static_final_host_xml = 0x7f35;
+	public static final int public_static_final_host_raw = 0x7f36;
+	public static final int public_static_final_host_dimen = 0x7f37;
+	public static final int public_static_final_host_string = 0x7f38;
+	public static final int public_static_final_host_style = 0x7f39;
+	public static final int public_static_final_host_color = 0x7f3a;
+	public static final int public_static_final_host_id = 0x7f3b;
+	public static final int public_static_final_host_bool = 0x7f3c;
+	public static final int public_static_final_host_int = 0x7f3d;
+	public static final int public_static_final_host_array = 0x7f3e;
+	public static final int public_static_final_host_menu = 0x7f3f;
 	
 	public static SparseArray<String> resourceMap = new SparseArray<String>(16);
 	
 	static {
-		resourceMap.put(main_attr, "attr");
-		resourceMap.put(main_drawable, "drawable");
-		resourceMap.put(main_layout, "layout");
-		resourceMap.put(main_anim, "anim");
-		resourceMap.put(main_xml, "xml");
-		resourceMap.put(main_raw, "raw");
-		resourceMap.put(main_dimen, "dimen");
-		resourceMap.put(main_string, "string");
-		resourceMap.put(main_style, "style");
-		resourceMap.put(main_color, "color");
-		resourceMap.put(main_id, "id");
-		resourceMap.put(main_bool, "bool");
-		resourceMap.put(main_int, "int");
-		resourceMap.put(main_array, "array");
-		resourceMap.put(main_menu, "menu");
+		resourceMap.put(public_static_final_host_attr, "attr");
+		resourceMap.put(public_static_final_host_drawable, "drawable");
+		resourceMap.put(public_static_final_host_layout, "layout");
+		resourceMap.put(public_static_final_host_anim, "anim");
+		resourceMap.put(public_static_final_host_xml, "xml");
+		resourceMap.put(public_static_final_host_raw, "raw");
+		resourceMap.put(public_static_final_host_dimen, "dimen");
+		resourceMap.put(public_static_final_host_string, "string");
+		resourceMap.put(public_static_final_host_style, "style");
+		resourceMap.put(public_static_final_host_color, "color");
+		resourceMap.put(public_static_final_host_id, "id");
+		resourceMap.put(public_static_final_host_bool, "bool");
+		resourceMap.put(public_static_final_host_int, "int");
+		resourceMap.put(public_static_final_host_array, "array");
+		resourceMap.put(public_static_final_host_menu, "menu");
 	}
 }
