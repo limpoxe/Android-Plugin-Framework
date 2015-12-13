a7c4b872da44e951f78a840def9596a8fe675df8
limpoxe <405977146@163.com>
2015-12-13 21:04:29 +0800

    Update Demo

--- PluginHelloWorld/src/main/java/com/example/pluginhelloworld/MainActivity.java
+++ PluginHelloWorld/src/main/java/com/example/pluginhelloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pluginhelloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 /**
  * 独立插件测试demo
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- PluginHelloWorld/src/main/java/com/example/pluginhelloworld/WelcomeActivity.java
+++ PluginHelloWorld/src/main/java/com/example/pluginhelloworld/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.pluginhelloworld;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,7 +15,7 @@ import com.example.hellojni.HelloJni;
 /**
  * 独立插件测试demo
  */
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
