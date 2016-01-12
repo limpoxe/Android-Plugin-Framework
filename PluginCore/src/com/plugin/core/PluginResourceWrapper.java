f37c0176a373e71111851400fcb18b9aba4988ea
liming.clm <liming.clm@alibaba-inc.com>
2016-01-12 21:06:36 +0800

    getIdentifier

--- PluginCore/src/com/plugin/core/PluginResourceWrapper.java
+++ PluginCore/src/com/plugin/core/PluginResourceWrapper.java
@@ -3,10 +3,13 @@ package com.plugin.core;
 import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 
 import com.plugin.content.PluginDescriptor;
 import com.plugin.util.LogUtil;
+
+import java.lang.reflect.Field;
 
 /**
  * 根据不同的rom，可能需要重写更多的方法，目前发现的几个机型的问题暂时只需要重写下面2个方法。
@@ -63,11 +66,74 @@ public class PluginResourceWrapper extends Resources {
 		return resid>>24 == 0x7f;
 	}
 
+
+	/**
+	 * 重写此方法主要是为了修正在插件中通过
+	 * getIdentifier(name, type,  getPackageName())此中形式反查id时
+	 *
+	 * 如果第三个参数通过调用getPackageName获得，由于此方法返回的是宿主的包名，可能会得不到预期的结果
+	 *
+	 * @param name
+	 * @param defType
+	 * @param defPackage
+	 * @return
+	 */
 	@Override
 	public int getIdentifier(String name, String defType, String defPackage) {
-		LogUtil.e("packageName", defPackage, "重置为：", mPluginDescriptor.getPackageName());
-		defPackage = mPluginDescriptor.getPackageName();
-		return super.getIdentifier(name, defType, defPackage);
+
+		if (TextUtils.isDigitsOnly(name)) {
+			return super.getIdentifier(name, defType, defPackage);
+		}
+
+		//传了packageName，而且不是宿主的packageName， 则直接返回
+		if (!TextUtils.isEmpty(defPackage) && !PluginLoader.getApplicatoin().getPackageName().equals(defPackage)) {
+			return super.getIdentifier(name, defType, defPackage);
+		}
+
+		//package:type/entry
+		//第一段 “package:“ 第二段 ”type/“ 第三段 “entry”
+		String packageName = null;
+		String type = null;
+		String entry = null;
+
+		String[] pte = name.split(":");
+		String[] te;
+		if (pte.length == 2) {
+			packageName = pte[0];
+			te = pte[1].split("/");
+		} else {
+			te = pte[0].split("/");
+		}
+
+		if (te.length == 2) {
+			type = te[0];
+			entry = te[1];
+		} else {
+			entry = te[0];
+		}
+
+		if (packageName == null) {
+			packageName = defPackage;
+		}
+
+		if (type == null) {
+			type = defType;
+		}
+
+		if (PluginLoader.getApplicatoin().getPackageName().equals(packageName)) {
+			// 判断是否在真的在宿主中
+			Class rClass = null;
+			try {
+				rClass = this.getClass().getClassLoader().loadClass(packageName + ".R$" + type);
+				Field field = rClass.getDeclaredField(entry);
+				if (field == null) {//不在宿主中，换成插件的
+					packageName = mPluginDescriptor.getPackageName();
+				}
+			} catch (Exception e) {
+			}
+		}
+		return super.getIdentifier(entry, type, packageName);
+
 	}
 }
 
