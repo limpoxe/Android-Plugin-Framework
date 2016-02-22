97f89b6585009335c93b01930c8e4244748ff049
cailiming <liming.clm@alibaba-inc.com>
2016-02-22 21:03:30 +0800

    优化api

--- PluginCore/src/com/plugin/core/PluginLoader.java
+++ PluginCore/src/com/plugin/core/PluginLoader.java
@@ -48,23 +48,11 @@ import dalvik.system.DexClassLoader;
 
 public class PluginLoader {
 
-	private static final boolean NEED_VERIFY_CERT = true;
-	private static final int SUCCESS = 0;
-	private static final int SRC_FILE_NOT_FOUND = 1;
-	private static final int COPY_FILE_FAIL = 2;
-	private static final int SIGNATURES_INVALIDATE = 3;
-	private static final int VERIFY_SIGNATURES_FAIL = 4;
-	private static final int PARSE_MANIFEST_FAIL = 5;
-	private static final int FAIL_BECAUSE_HAS_LOADED = 6;
-	private static final int INSTALL_FAIL = 7;
-
 	private static Application sApplication;
 
 	private static boolean isLoaderInited = false;
 
 	private static PluginManager pluginManager;
-
-	private static PluginCallback changeListener;
 
 	private PluginLoader() {
 	}
@@ -86,7 +74,6 @@ public class PluginLoader {
 			isLoaderInited = true;
 			sApplication = app;
 			pluginManager = manager;
-			changeListener = new PluginCallbackImpl();
 
 			AndroidAppIActivityManager.installProxy();
 			AndroidAppINotificationManager.installProxy();
@@ -111,8 +98,6 @@ public class PluginLoader {
 				PluginDescriptor plugin = itr.next();
 				LocalServiceManager.registerService(plugin);
 			}
-			changeListener.onPluginLoaderInited();
-
 
 			if (Build.VERSION.SDK_INT >= 14) {
 				sApplication.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
@@ -158,156 +143,7 @@ public class PluginLoader {
 		}
 	}
 
-	/**
-	 * 安装一个插件
-	 * 
-	 * @param srcPluginFile
-	 * @return
-	 */
-	public static synchronized int installPlugin(String srcPluginFile) {
-		LogUtil.e("开始安装插件", srcPluginFile);
-		if (TextUtils.isEmpty(srcPluginFile) || !new File(srcPluginFile).exists()) {
-			return SRC_FILE_NOT_FOUND;
-		}
 
-		//第0步，先将apk复制到宿主程序私有目录，防止在安装过程中文件被篡改
-		if (!srcPluginFile.startsWith(sApplication.getCacheDir().getAbsolutePath())) {
-			String tempFilePath = sApplication.getCacheDir().getAbsolutePath()
-					+ File.separator + System.currentTimeMillis() + ".apk";
-			if (FileUtil.copyFile(srcPluginFile, tempFilePath)) {
-				srcPluginFile = tempFilePath;
-			} else {
-				LogUtil.e("复制插件文件失败", srcPluginFile, tempFilePath);
-				return COPY_FILE_FAIL;
-			}
-		}
-
-		// 第1步，验证插件APK签名，如果被篡改过，将获取不到证书
-		//sApplication.getPackageManager().getPackageArchiveInfo(srcPluginFile, PackageManager.GET_SIGNATURES);
-		Signature[] pluginSignatures = PackageVerifyer.collectCertificates(srcPluginFile, false);
-		boolean isDebugable = (0 != (sApplication.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
-		if (pluginSignatures == null) {
-			LogUtil.e("插件签名验证失败", srcPluginFile);
-			new File(srcPluginFile).delete();
-			return SIGNATURES_INVALIDATE;
-		} else if (NEED_VERIFY_CERT && !isDebugable) {
-			//可选步骤，验证插件APK证书是否和宿主程序证书相同。
-			//证书中存放的是公钥和算法信息，而公钥和私钥是1对1的
-			//公钥相同意味着是同一个作者发布的程序
-			Signature[] mainSignatures = null;
-			try {
-				PackageInfo pkgInfo = sApplication.getPackageManager().getPackageInfo(sApplication.getPackageName(), PackageManager.GET_SIGNATURES);
-				mainSignatures = pkgInfo.signatures;
-			} catch (PackageManager.NameNotFoundException e) {
-				e.printStackTrace();
-			}
-			if (!PackageVerifyer.isSignaturesSame(mainSignatures, pluginSignatures)) {
-				LogUtil.e("插件证书和宿主证书不一致", srcPluginFile);
-				new File(srcPluginFile).delete();
-				return VERIFY_SIGNATURES_FAIL;
-			}
-		}
-
-		// 第2步，解析Manifest，获得插件详情
-		PluginDescriptor pluginDescriptor = PluginManifestParser.parseManifest(srcPluginFile);
-		if (pluginDescriptor == null || TextUtils.isEmpty(pluginDescriptor.getPackageName())) {
-			LogUtil.e("解析插件Manifest文件失败", srcPluginFile);
-			new File(srcPluginFile).delete();
-			return PARSE_MANIFEST_FAIL;
-		}
-
-		PackageInfo packageInfo = sApplication.getPackageManager().getPackageArchiveInfo(srcPluginFile, PackageManager.GET_GIDS);
-		if (packageInfo != null) {
-			pluginDescriptor.setApplicationTheme(packageInfo.applicationInfo.theme);
-			pluginDescriptor.setApplicationIcon(packageInfo.applicationInfo.icon);
-			pluginDescriptor.setApplicationLogo(packageInfo.applicationInfo.logo);
-		}
-
-		boolean isNeedPending = false;
-		// 第3步，检查插件是否已经存在,若存在删除旧的
-		PluginDescriptor oldPluginDescriptor = getPluginDescriptorByPluginId(pluginDescriptor.getPackageName());
-		if (oldPluginDescriptor != null) {
-			LogUtil.e("已安装过，安装路径为", oldPluginDescriptor.getInstalledPath(), oldPluginDescriptor.getVersion(), pluginDescriptor.getVersion());
-
-			//检查插件是否已经加载
-			if (oldPluginDescriptor.getPluginContext() != null) {
-				if (!oldPluginDescriptor.getVersion().equals(pluginDescriptor.getVersion())) {
-					LogUtil.e("旧版插件已经加载， 且新版插件和旧版插件版本不同，进入pending状态，新版插件将在安装后进程重启再生效");
-					isNeedPending = true;
-				} else {
-					LogUtil.e("旧版插件已经加载， 且新版插件和旧版插件版本相同，拒绝安装");
-					new File(srcPluginFile).delete();
-					return FAIL_BECAUSE_HAS_LOADED;
-				}
-			} else {
-				LogUtil.e("旧版插件还未加载，忽略版本，直接删除旧版，尝试安装新版");
-				remove(oldPluginDescriptor.getPackageName());
-			}
-		}
-
-		// 第4步骤，复制插件到插件目录
-		String destApkPath = pluginManager.genInstallPath(pluginDescriptor.getPackageName(), pluginDescriptor.getVersion());
-		boolean isCopySuccess = FileUtil.copyFile(srcPluginFile, destApkPath);
-
-		if (!isCopySuccess) {
-
-			LogUtil.e("复制插件到安装目录失败", srcPluginFile);
-			//删掉临时文件
-			new File(srcPluginFile).delete();
-			return COPY_FILE_FAIL;
-		} else {
-
-			//第5步，先解压so到临时目录，再从临时目录复制到插件so目录。 在构造插件Dexclassloader的时候，会使用这个so目录作为参数
-			File apkParent = new File(destApkPath).getParentFile();
-			File tempSoDir = new File(apkParent, "temp");
-			Set<String> soList = FileUtil.unZipSo(srcPluginFile, tempSoDir);
-			if (soList != null) {
-				for (String soName : soList) {
-					FileUtil.copySo(tempSoDir, soName, apkParent.getAbsolutePath());
-				}
-				//删掉临时文件
-				FileUtil.deleteAll(tempSoDir);
-			}
-
-			// 第6步 添加到已安装插件列表
-			pluginDescriptor.setInstalledPath(destApkPath);
-			boolean isInstallSuccess = false;
-			if (!isNeedPending) {
-				isInstallSuccess = pluginManager.addOrReplace(pluginDescriptor);
-			} else {
-				isInstallSuccess = pluginManager.pending(pluginDescriptor);
-			}
-			//删掉临时文件
-			new File(srcPluginFile).delete();
-
-			if (!isInstallSuccess) {
-				LogUtil.e("安装插件失败", srcPluginFile);
-
-				new File(destApkPath).delete();
-
-				return INSTALL_FAIL;
-			} else {
-				//通过创建classloader来触发dexopt，但不加载
-				LogUtil.d("正在进行DEXOPT...", pluginDescriptor.getInstalledPath());
-				//ActivityThread.getPackageManager().performDexOptIfNeeded()
-				FileUtil.deleteAll(new File(apkParent, "dalvik-cache"));
-				PluginCreator.createPluginClassLoader(pluginDescriptor.getInstalledPath(), pluginDescriptor.isStandalone(), null);
-				LogUtil.d("DEXOPT完毕");
-
-				if (!isNeedPending) {
-					LocalServiceManager.registerService(pluginDescriptor);
-				}
-
-				changeListener.onPluginInstalled(pluginDescriptor.getPackageName(), pluginDescriptor.getVersion());
-				LogUtil.e("安装插件成功," + (isNeedPending ? " 重启进程生效" : " 立即生效"), destApkPath);
-
-				//打印一下目录结构
-				FileUtil.printAll(new File(PluginLoader.getApplicatoin().getApplicationInfo().dataDir));
-
-				return SUCCESS;
-			}
-		}
-	}
 
 	/**
 	 * 通过插件Id唤起插件
@@ -505,8 +341,6 @@ public class PluginLoader {
 					e.printStackTrace();
 				}
 
-				changeListener.onPluginStarted(pluginDescriptor.getPackageName());
-
 				LogUtil.e("初始化插件" + pluginDescriptor.getPackageName() + "完成");
 			}
 		}
@@ -557,17 +391,11 @@ public class PluginLoader {
 	 * 清除列表并不能清除已经加载到内存当中的class,因为class一旦加载后后无法卸载
 	 */
 	public static synchronized void removeAll() {
-		boolean isSuccess = pluginManager.removeAll();
-		if (isSuccess) {
-			changeListener.onPluginRemoveAll();
-		}
+		pluginManager.removeAll();
 	}
 
 	public static synchronized void remove(String pluginId) {
-		boolean isSuccess = pluginManager.remove(pluginId);
-		if (isSuccess) {
-			changeListener.onPluginRemoved(pluginId);
-		}
+		pluginManager.remove(pluginId);
 	}
 
 	@SuppressWarnings("unchecked")
--- PluginCore/src/com/plugin/core/manager/PluginManagerImpl.java
+++ PluginCore/src/com/plugin/core/manager/PluginManagerImpl.java
@@ -2,14 +2,22 @@ package com.plugin.core.manager;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.text.TextUtils;
 import android.util.Base64;
 
 import com.plugin.content.PluginDescriptor;
+import com.plugin.core.PluginCreator;
 import com.plugin.core.PluginLoader;
+import com.plugin.core.PluginManifestParser;
+import com.plugin.core.localservice.LocalServiceManager;
 import com.plugin.util.FileUtil;
 import com.plugin.util.LogUtil;
+import com.plugin.util.PackageVerifyer;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -22,8 +30,19 @@ import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class PluginManagerImpl implements PluginManager {
+
+	private static final boolean NEED_VERIFY_CERT = true;
+	private static final int SUCCESS = 0;
+	private static final int SRC_FILE_NOT_FOUND = 1;
+	private static final int COPY_FILE_FAIL = 2;
+	private static final int SIGNATURES_INVALIDATE = 3;
+	private static final int VERIFY_SIGNATURES_FAIL = 4;
+	private static final int PARSE_MANIFEST_FAIL = 5;
+	private static final int FAIL_BECAUSE_HAS_LOADED = 6;
+	private static final int INSTALL_FAIL = 7;
 
 	private static final String INSTALLED_KEY = "plugins.list";
 
@@ -32,6 +51,8 @@ public class PluginManagerImpl implements PluginManager {
 	private final Hashtable<String, PluginDescriptor> sInstalledPlugins = new Hashtable<String, PluginDescriptor>();
 
 	private final Hashtable<String, PluginDescriptor> sPendingPlugins = new Hashtable<String, PluginDescriptor>();
+
+	private PluginCallback changeListener = new PluginCallbackImpl();
 
 	/**
 	 * 插件的安装目录, 插件apk将来会被放在这个目录下面
@@ -87,19 +108,25 @@ public class PluginManagerImpl implements PluginManager {
 	public synchronized boolean removeAll() {
 		sInstalledPlugins.clear();
 		boolean isSuccess = savePlugins(INSTALLED_KEY, sInstalledPlugins);
+
+		changeListener.onPluginRemoveAll();
+
 		return isSuccess;
 	}
 
 	@Override
 	public synchronized boolean remove(String pluginId) {
 		PluginDescriptor old = sInstalledPlugins.remove(pluginId);
+		boolean result = false;
 		if (old != null) {
-			boolean isSuccess = savePlugins(INSTALLED_KEY, sInstalledPlugins);
+			result = savePlugins(INSTALLED_KEY, sInstalledPlugins);
 			boolean deleteSuccess = FileUtil.deleteAll(new File(old.getInstalledPath()).getParentFile());
-			LogUtil.d("delete old", isSuccess, deleteSuccess, old.getInstalledPath(), old.getPackageName());
-			return isSuccess;
+			LogUtil.d("delete old", result, deleteSuccess, old.getInstalledPath(), old.getPackageName());
 		}
-		return false;
+
+		changeListener.onPluginRemoved(pluginId);
+
+		return result;
 	}
 
 	@Override
@@ -153,6 +180,157 @@ public class PluginManagerImpl implements PluginManager {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * 安装一个插件
+	 *
+	 * @param srcPluginFile
+	 * @return
+	 */
+	public synchronized int installPlugin(String srcPluginFile) {
+		LogUtil.e("开始安装插件", srcPluginFile);
+		if (TextUtils.isEmpty(srcPluginFile) || !new File(srcPluginFile).exists()) {
+			return SRC_FILE_NOT_FOUND;
+		}
+
+		//第0步，先将apk复制到宿主程序私有目录，防止在安装过程中文件被篡改
+		if (!srcPluginFile.startsWith(PluginLoader.getApplicatoin().getCacheDir().getAbsolutePath())) {
+			String tempFilePath = PluginLoader.getApplicatoin().getCacheDir().getAbsolutePath()
+					+ File.separator + System.currentTimeMillis() + ".apk";
+			if (FileUtil.copyFile(srcPluginFile, tempFilePath)) {
+				srcPluginFile = tempFilePath;
+			} else {
+				LogUtil.e("复制插件文件失败", srcPluginFile, tempFilePath);
+				return COPY_FILE_FAIL;
+			}
+		}
+
+		// 第1步，验证插件APK签名，如果被篡改过，将获取不到证书
+		//sApplication.getPackageManager().getPackageArchiveInfo(srcPluginFile, PackageManager.GET_SIGNATURES);
+		Signature[] pluginSignatures = PackageVerifyer.collectCertificates(srcPluginFile, false);
+		boolean isDebugable = (0 != (PluginLoader.getApplicatoin().getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
+		if (pluginSignatures == null) {
+			LogUtil.e("插件签名验证失败", srcPluginFile);
+			new File(srcPluginFile).delete();
+			return SIGNATURES_INVALIDATE;
+		} else if (NEED_VERIFY_CERT && !isDebugable) {
+			//可选步骤，验证插件APK证书是否和宿主程序证书相同。
+			//证书中存放的是公钥和算法信息，而公钥和私钥是1对1的
+			//公钥相同意味着是同一个作者发布的程序
+			Signature[] mainSignatures = null;
+			try {
+				PackageInfo pkgInfo = PluginLoader.getApplicatoin().getPackageManager().getPackageInfo(PluginLoader.getApplicatoin().getPackageName(), PackageManager.GET_SIGNATURES);
+				mainSignatures = pkgInfo.signatures;
+			} catch (PackageManager.NameNotFoundException e) {
+				e.printStackTrace();
+			}
+			if (!PackageVerifyer.isSignaturesSame(mainSignatures, pluginSignatures)) {
+				LogUtil.e("插件证书和宿主证书不一致", srcPluginFile);
+				new File(srcPluginFile).delete();
+				return VERIFY_SIGNATURES_FAIL;
+			}
+		}
+
+		// 第2步，解析Manifest，获得插件详情
+		PluginDescriptor pluginDescriptor = PluginManifestParser.parseManifest(srcPluginFile);
+		if (pluginDescriptor == null || TextUtils.isEmpty(pluginDescriptor.getPackageName())) {
+			LogUtil.e("解析插件Manifest文件失败", srcPluginFile);
+			new File(srcPluginFile).delete();
+			return PARSE_MANIFEST_FAIL;
+		}
+
+		PackageInfo packageInfo = PluginLoader.getApplicatoin().getPackageManager().getPackageArchiveInfo(srcPluginFile, PackageManager.GET_GIDS);
+		if (packageInfo != null) {
+			pluginDescriptor.setApplicationTheme(packageInfo.applicationInfo.theme);
+			pluginDescriptor.setApplicationIcon(packageInfo.applicationInfo.icon);
+			pluginDescriptor.setApplicationLogo(packageInfo.applicationInfo.logo);
+		}
+
+		boolean isNeedPending = false;
+		// 第3步，检查插件是否已经存在,若存在删除旧的
+		PluginDescriptor oldPluginDescriptor = getPluginDescriptorByPluginId(pluginDescriptor.getPackageName());
+		if (oldPluginDescriptor != null) {
+			LogUtil.e("已安装过，安装路径为", oldPluginDescriptor.getInstalledPath(), oldPluginDescriptor.getVersion(), pluginDescriptor.getVersion());
+
+			//检查插件是否已经加载
+			if (oldPluginDescriptor.getPluginContext() != null) {
+				if (!oldPluginDescriptor.getVersion().equals(pluginDescriptor.getVersion())) {
+					LogUtil.e("旧版插件已经加载， 且新版插件和旧版插件版本不同，进入pending状态，新版插件将在安装后进程重启再生效");
+					isNeedPending = true;
+				} else {
+					LogUtil.e("旧版插件已经加载， 且新版插件和旧版插件版本相同，拒绝安装");
+					new File(srcPluginFile).delete();
+					return FAIL_BECAUSE_HAS_LOADED;
+				}
+			} else {
+				LogUtil.e("旧版插件还未加载，忽略版本，直接删除旧版，尝试安装新版");
+				remove(oldPluginDescriptor.getPackageName());
+			}
+		}
+
+		// 第4步骤，复制插件到插件目录
+		String destApkPath = genInstallPath(pluginDescriptor.getPackageName(), pluginDescriptor.getVersion());
+		boolean isCopySuccess = FileUtil.copyFile(srcPluginFile, destApkPath);
+
+		if (!isCopySuccess) {
+
+			LogUtil.e("复制插件到安装目录失败", srcPluginFile);
+			//删掉临时文件
+			new File(srcPluginFile).delete();
+			return COPY_FILE_FAIL;
+		} else {
+
+			//第5步，先解压so到临时目录，再从临时目录复制到插件so目录。 在构造插件Dexclassloader的时候，会使用这个so目录作为参数
+			File apkParent = new File(destApkPath).getParentFile();
+			File tempSoDir = new File(apkParent, "temp");
+			Set<String> soList = FileUtil.unZipSo(srcPluginFile, tempSoDir);
+			if (soList != null) {
+				for (String soName : soList) {
+					FileUtil.copySo(tempSoDir, soName, apkParent.getAbsolutePath());
+				}
+				//删掉临时文件
+				FileUtil.deleteAll(tempSoDir);
+			}
+
+			// 第6步 添加到已安装插件列表
+			pluginDescriptor.setInstalledPath(destApkPath);
+			boolean isInstallSuccess = false;
+			if (!isNeedPending) {
+				isInstallSuccess = addOrReplace(pluginDescriptor);
+			} else {
+				isInstallSuccess = pending(pluginDescriptor);
+			}
+			//删掉临时文件
+			new File(srcPluginFile).delete();
+
+			if (!isInstallSuccess) {
+				LogUtil.e("安装插件失败", srcPluginFile);
+
+				new File(destApkPath).delete();
+
+				return INSTALL_FAIL;
+			} else {
+				//通过创建classloader来触发dexopt，但不加载
+				LogUtil.d("正在进行DEXOPT...", pluginDescriptor.getInstalledPath());
+				//ActivityThread.getPackageManager().performDexOptIfNeeded()
+				FileUtil.deleteAll(new File(apkParent, "dalvik-cache"));
+				PluginCreator.createPluginClassLoader(pluginDescriptor.getInstalledPath(), pluginDescriptor.isStandalone(), null);
+				LogUtil.d("DEXOPT完毕");
+
+				if (!isNeedPending) {
+					LocalServiceManager.registerService(pluginDescriptor);
+				}
+
+				changeListener.onPluginInstalled(pluginDescriptor.getPackageName(), pluginDescriptor.getVersion());
+				LogUtil.e("安装插件成功," + (isNeedPending ? " 重启进程生效" : " 立即生效"), destApkPath);
+
+				//打印一下目录结构
+				FileUtil.printAll(new File(PluginLoader.getApplicatoin().getApplicationInfo().dataDir));
+
+				return SUCCESS;
+			}
+		}
 	}
 
 	private static SharedPreferences getSharedPreference() {
