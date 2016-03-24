594bb96436d4636c3a0c23413c75dc2a2f8bf4c9
cailiming <liming.clm@alibaba-inc.com>
2016-03-24 16:48:31 +0800

    Update Demo

--- PluginTest/src/com/example/plugintest/PluginTestApplication.java
+++ PluginTest/src/com/example/plugintest/PluginTestApplication.java
@@ -1,11 +1,20 @@
 package com.example.plugintest;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.plugin.util.LogUtil;
+
 public class PluginTestApplication extends  Application {
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		Context ctx = getApplicationContext();
+		LogUtil.d("ctx", ctx);
 	}
+
+
 
 }
