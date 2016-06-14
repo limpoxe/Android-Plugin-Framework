98d2b6ca80630ec187331a2fbb4cbe86543b09d9
cailiming <liming.clm@alibaba-inc.com>
2016-06-14 18:13:57 +0800

    Bug Fix

--- PluginCore/src/com/plugin/core/PluginInjector.java
+++ PluginCore/src/com/plugin/core/PluginInjector.java
@@ -347,7 +347,9 @@ public class PluginInjector {
 	}
 
 	/**
-	 * 主题的选择顺序为 先选择插件Activity配置的主题，再选择插件Application配置的主题，再选择宿主Activity主题
+	 * 主题的选择顺序为 先选择插件Activity配置的主题，再选择插件Application配置的主题，
+	 * 如果是非独立插件，再选择宿主Activity主题
+	 * 如果是独立插件，再选择系统默认主题
 	 * @param activityInfo
 	 * @param pluginActivityInfo
 	 * @param pd
@@ -361,6 +363,11 @@ public class PluginInjector {
 		if (pluginAppTheme == 0) {
 			pluginAppTheme = pd.getApplicationTheme();
 		}
+
+		if (pluginAppTheme == 0 && pd.isStandalone()) {
+			pluginAppTheme = android.R.style.Theme_DeviceDefault;
+		}
+
 		if (pluginAppTheme == 0) {
 			//If the activity defines a theme, that is used; else, the application theme is used.
 			pluginAppTheme = activityInfo.getThemeResource();
