19e8ef040db73220417666541115caff1c814294
cailiming <liming.clm@alibaba-inc.com>
2016-08-30 13:08:37 +0800

    fixbug

--- PluginCore/src/main/java/com/plugin/core/compat/CompatForSupportv7_23_2.java
+++ PluginCore/src/main/java/com/plugin/core/compat/CompatForSupportv7_23_2.java
@@ -4,14 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.res.Resources;
-import android.view.View;
 
 import com.plugin.util.RefInvoker;
 
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Constructor;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by cailiming on 16/4/14.
@@ -19,31 +16,38 @@ import java.util.HashMap;
 public class CompatForSupportv7_23_2 {
 
     /**
-     *  //supportv7的23.2的版本中，AppCompatActivity内部重写了getResource方法，这里需要特别处理一下, 否者接下来的setTheme方法会导致crash
-     *   //其他版本，包括更低和更高版本的AppCompatActivity都不存在这个问题
+     *  supportv7的23.2的版本中，AppCompatActivity这个类重写了getResource方法，返回了一个TintResources的对象
+     *  这里需要特别处理一下, 否者接下来的setTheme方法会导致crash
+     *  其他版本，包括更低和更高版本的AppCompatActivity都没有重写这个方法.
+     *  <Pre>
+     *       public Resources getResources() {
+     *           if (mResources == null) {
+     *               mResources = new TintResources(this, super.getResources());
+     *           }
+     *           return mResources;
+     *       }
+     *  </Pre>
      * @param pluginContext
      * @param activity
      */
     public static void fixResource(Context pluginContext, Activity activity) {
         try {
-            //只有23.2的版本中存在此类
-            Class TintResources = pluginContext.getClassLoader().loadClass("android.support.v7.widget.TintResources");
-            if (TintResources != null) {
-                Class AppCompatActivity = pluginContext.getClassLoader().loadClass("android.support.v7.app.AppCompatActivity");
-                if (AppCompatActivity != null && AppCompatActivity.isAssignableFrom(activity.getClass())) {
+            Class AppCompatActivity = pluginContext.getClassLoader().loadClass("android.support.v7.app.AppCompatActivity");
+            if (AppCompatActivity != null && AppCompatActivity.isAssignableFrom(activity.getClass())) {
+                //判断Activity的getResource的类型是否为TintResources
+                Resources activiyResource = activity.getResources();
+                Class TintResources = pluginContext.getClassLoader().loadClass("android.support.v7.widget.TintResources");
+                if (TintResources != null && TintResources.isAssignableFrom(activiyResource.getClass())) {
                     RefInvoker.setFieldObject(activity, AppCompatActivity, "mResources", pluginContext.getResources());
-
                     Class TintContextWrapper = pluginContext.getClassLoader().loadClass("android.support.v7.widget.TintContextWrapper");
                     if (TintContextWrapper != null) {
                         Object sCache = (Object)RefInvoker.getFieldObject(null, TintContextWrapper, "sCache");
                         if (!(sCache instanceof TintContextWrapperArrayList)) {
-                            RefInvoker.setFieldObject(null, TintContextWrapper, "sCache",
-                                    new TintContextWrapperArrayList(TintContextWrapper, pluginContext.getResources()));
+                            RefInvoker.setFieldObject(null, TintContextWrapper, "sCache", new TintContextWrapperArrayList(TintContextWrapper));
                         }
                     }
                 }
             }
-
         } catch (ClassNotFoundException e) {
             //nothing
         }
@@ -52,11 +56,9 @@ public class CompatForSupportv7_23_2 {
     public static class TintContextWrapperArrayList<V> extends ArrayList<V> {
 
         private Class TintContextWrapper;
-        private Resources resources;
 
-        public TintContextWrapperArrayList(Class TintContextWrapper, Resources resources) {
+        public TintContextWrapperArrayList(Class TintContextWrapper) {
             this.TintContextWrapper = TintContextWrapper;
-            this.resources = resources;
         }
 
         @Override
@@ -64,6 +66,7 @@ public class CompatForSupportv7_23_2 {
             WeakReference ref = (WeakReference)object;
             Object tintContextWrapper = ref.get();
             if (tintContextWrapper != null) {
+                Resources resources = ((ContextWrapper)tintContextWrapper).getBaseContext().getResources();
                 RefInvoker.setFieldObject(tintContextWrapper, TintContextWrapper, "mResources", resources);
                 Resources.Theme theme = resources.newTheme();
                 theme.setTo(((ContextWrapper)tintContextWrapper).getBaseContext().getTheme());
