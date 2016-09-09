f1a85abd660e6e538880be1111962e6297c94d44
jiacheng.gjc <jiacheng.gjc@alibaba-inc.com>
2016-09-09 15:05:35 +0800

    gu for test

--- PluginBase/src/main/java/com/example/plugintestbase/LoginVO.java
+++ PluginBase/src/main/java/com/example/plugintestbase/LoginVO.java
@@ -11,10 +11,10 @@ public class LoginVO implements Serializable {
 
     private String password;
 
-
     public String getUsername() {
         return username;
     }
+
 
     public void setUsername(String username) {
         this.username = username;
