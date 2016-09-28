7e82235372acd0d0e0621897d1df4dee457a2eb8
cailiming <liming.clm@alibaba-inc.com>
2016-09-29 01:01:28 +0800

    修改AppOpsService的checkPackage递归异常

--- PluginCore/src/main/java/com/plugin/core/systemservice/BinderProxyDelegate.java
+++ PluginCore/src/main/java/com/plugin/core/systemservice/BinderProxyDelegate.java
@@ -25,7 +25,10 @@ public class BinderProxyDelegate extends MethodDelegate {
 
         //这里做此判定是为了把一些特定的接口方法仍然交给特定的MethodProxy去处理,不在此做统一处理
         //这些"特定的MethodProxy"主要是一些查询类接口
-        if(!MethodProxy.sMethods.containsKey(method.getName())) {
+        //单独判断checkPackage是因为AppOpsService的checkPackage方法会进入这里, 而if里面的replacePackageName方法里
+        // 面会触发一次ContentProvider调用, ContentProvider调用又会触发AppOpsService的checkPackage方法,
+        // AppOpsService的checkPackage方法被触发后又回进入这里, 造成递归异常,因此这里单独屏蔽掉checkPackage方法
+        if (!MethodProxy.sMethods.containsKey(method.getName()) && !"checkPackage".equals(method.getName())) {
             replacePackageName(args);
         }
 
@@ -42,14 +45,17 @@ public class BinderProxyDelegate extends MethodDelegate {
             for (int i = 0; i < args.length; i++) {
                 if (args[i] instanceof  String && ((String)args[i]).contains(".")) {
                     // 包含.号,基本可以判定是packageName
-                    PluginDescriptor pd = PluginManagerHelper.getPluginDescriptorByPluginId((String)args[i]);
-                    if(pd != null) {
-                        // 说明传的是插件包名, 修正为宿主包名
-                        args[i] = PluginLoader.getApplication().getPackageName();
+                    if (!args[i].equals(PluginLoader.getApplication().getPackageName())) {
+                        //下面这个方法调用会触发ContentProvider
+                        PluginDescriptor pd = PluginManagerHelper.getPluginDescriptorByPluginId((String)args[i]);
+                        if(pd != null) {
+                            // 说明传的是插件包名, 修正为宿主包名
+                            args[i] = PluginLoader.getApplication().getPackageName();
 
-                        // 这里或许需要break,提高效率,
-                        // 因为一个接口的参数里面出现两个packageName的可能性较小
-                        // break;
+                            // 这里或许需要break,提高效率,
+                            // 因为一个接口的参数里面出现两个packageName的可能性较小
+                            // break;
+                        }
                     }
                 }
             }
