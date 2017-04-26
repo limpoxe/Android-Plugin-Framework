a6d096d5a8f11ea87564236764e4b0c9a3d347a7
cailiming <liming.clm@alibaba-inc.com>
2017-04-26 23:12:49 +0800

    update demo

---
+++ Samples/PluginTest/src/main/java/com/example/plugintest/receiver/BootCompletedReceiver.java
@@ -0,0 +1,15 @@
+package com.example.plugintest.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.limpoxe.fairy.util.LogUtil;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LogUtil.e(intent);
+    }
+}
