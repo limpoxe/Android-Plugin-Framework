3b53bc857cd06555b76026f0b50161dabd34d009
cailiming <liming.clm@alibaba-inc.com>
2017-12-22 22:55:59 +0800

    rename demo project

--- Samples/PluginMain/src/main/java/com/example/pluginmain/MyApplication.java
+++ Samples/PluginMain/src/main/java/com/example/pluginmain/MyApplication.java
@@ -2,6 +2,7 @@ package com.example.pluginmain;
 
 import com.limpoxe.fairy.core.FairyGlobal;
 import com.limpoxe.fairy.core.PluginApplication;
+import com.tencent.bugly.crashreport.CrashReport;
 //import com.tencent.bugly.crashreport.CrashReport;
 //import com.umeng.analytics.MobclickAgent;
 
@@ -11,7 +12,7 @@ public class MyApplication extends PluginApplication {
 		super.onCreate();
 
         //bugly SDK
-        //CrashReport.initCrashReport(getApplicationContext(), "c38ae3f8a6", true);
+        CrashReport.initCrashReport(getApplicationContext(), "c38ae3f8a6", true);
 
         //UMENG SDK
         //MobclickAgent.setDebugMode(true);
