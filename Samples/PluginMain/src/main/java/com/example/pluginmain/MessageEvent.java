e5594e802f8c65923d3fdd914fc3f8cf12c33042
cailiming <liming.clm@alibaba-inc.com>
2018-01-18 15:28:53 +0800

    update demo

---
+++ Samples/PluginMain/src/main/java/com/example/pluginmain/MessageEvent.java
@@ -0,0 +1,18 @@
+package com.example.pluginmain;
+
+public class MessageEvent {
+
+    private String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
