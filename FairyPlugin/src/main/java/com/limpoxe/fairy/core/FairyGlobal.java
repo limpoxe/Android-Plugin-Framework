ff1a5bfea029c4f017442a8b378360be97acfb24
limpoxe <405977146@163.com>
2021-04-18 18:07:33 +0800

    Update FairyGlobal.java

--- FairyPlugin/src/main/java/com/limpoxe/fairy/core/FairyGlobal.java
+++ FairyPlugin/src/main/java/com/limpoxe/fairy/core/FairyGlobal.java
@@ -2,8 +2,10 @@ package com.limpoxe.fairy.core;
 
 import android.app.Application;
 
+import com.limpoxe.fairy.core.exception.PluginResInitError;
 import com.limpoxe.fairy.manager.mapping.StubMappingProcessor;
 import com.limpoxe.fairy.util.LogUtil;
+import com.limpoxe.fairy.util.ResourceUtil;
 
 import java.util.ArrayList;
 
@@ -28,6 +30,11 @@ public class FairyGlobal {
 
     /*package*/ static void setApplication(Application application) {
         sApplication = application;
+        //public_static_final_host_string_这个资源是由框架脚本植入宿主的, 由此判断对宿主的id分组是否成功
+        int id = sApplication.getResources().getIdentifier("public_static_final_host_string_", "string", sApplication.getPackageName());
+        if (id != 0 && !ResourceUtil.isMainResId(id)) {
+            throw new PluginResInitError("对宿主id分组失败，说明宿主编译有错，请clean后重新编译: " + ResourceUtil.covent2Hex(String.valueOf(id)));
+        }
     }
 
     /*package*/ static void setIsInited(boolean isInited) {
