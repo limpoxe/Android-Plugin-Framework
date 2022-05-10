ca0c8206928482dbd46de0afd9770f91fddeb740
limpoxe <fixerror@163.com>
2022-05-10 22:19:12 +0800

    add kotlin to demo

--- Samples/PluginMain/src/main/java/com/example/pluginmain/MessageEvent.kt
+++ Samples/PluginMain/src/main/java/com/example/pluginmain/MessageEvent.kt
@@ -1,18 +1,3 @@
-package com.example.pluginmain;
+package com.example.pluginmain
 
-public class MessageEvent {
-
-    private String message;
-
-    public MessageEvent(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+class MessageEvent(var message: String)
--- Samples/PluginTest/src/main/java/com/example/plugintest/PluginTestApplication.kt
+++ Samples/PluginTest/src/main/java/com/example/plugintest/PluginTestApplication.kt
@@ -1,55 +1,39 @@
-package com.example.plugintest;
+package com.example.plugintest
 
-import android.app.ActivityManager;
-import android.app.Application;
-import android.content.Context;
-import androidx.multidex.MultiDex;
+import android.app.ActivityManager
+import android.app.ActivityManager.RunningAppProcessInfo
+import android.app.Application
+import android.content.Context
+import android.os.Process
+import androidx.multidex.MultiDex
+import com.limpoxe.fairy.util.FakeUtil
+import com.limpoxe.fairy.util.LogUtil
 
-import com.limpoxe.fairy.util.FakeUtil;
-import com.limpoxe.fairy.util.LogUtil;
+class PluginTestApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("PluginTestApplication onCreate, $applicationContext")
+        printProcessName()
+    }
 
-import java.util.List;
+    private fun printProcessName() {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val list = activityManager.runningAppProcesses
+        for (appProcess in list) {
+            if (appProcess.pid == Process.myPid()) {
+                Log.d("PluginTestApplication processName=" + appProcess.processName)
+                break
+            }
+        }
+    }
 
-public class PluginTestApplication extends  Application {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(FakeUtil.fakeMultiDexContext(this))
+    }
 
-	private String packageName;
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-
-		Context ctx = getApplicationContext();
-		Log.d("PluginTestApplication", "" + ctx);
-
-		if (isApplicationProcess()) {
-			Log.d("api欺骗成功，让插件以为自己在主进程运行");
-		}
-
-	}
-
-	private boolean isApplicationProcess() {
-		ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		List<ActivityManager.RunningAppProcessInfo> list = activityManager.getRunningAppProcesses();
-		for (ActivityManager.RunningAppProcessInfo appProcess : list) {
-			if (appProcess.pid == android.os.Process.myPid()) {
-				if (appProcess.processName.equals(packageName)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	@Override
-	protected void attachBaseContext(Context base) {
-		super.attachBaseContext(base);
-		packageName = getPackageName();
-		MultiDex.install(FakeUtil.fakeMultiDexContext(this));
-	}
-
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-		LogUtil.e("PluginTestApplication onTerminate");
-	}
+    override fun onTerminate() {
+        super.onTerminate()
+        LogUtil.e("PluginTestApplication onTerminate")
+    }
 }
